cd4a76c60c10aa0e55726ccae041af1d4ea074a1
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2023-08-26 16:02:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import DisplayFunction from './pages/display-page';
+import { Header } from './components/header';
+
+function App() {
+  return (
+    <div>
+    <DisplayFunction/>
+    </div>
+  );
+}
+
+export default App;
