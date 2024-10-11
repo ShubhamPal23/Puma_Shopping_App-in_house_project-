e9bb79ab10239fca8be185af69e0ed272dda70ce
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-10-11 20:27:59 +0530

    Update head.jsx

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,24 +1,9 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
 
-export default function Head({classname}) {
+export default function Head({ classname }) {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            {/* <MenuIcon /> */}
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            {classname}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <header style={{ background: '#000000', padding: '16px' }}>
+      <h5 style={{ color: 'white', margin: 0 }}>{classname}</h5>
+    </header>
   );
 }
