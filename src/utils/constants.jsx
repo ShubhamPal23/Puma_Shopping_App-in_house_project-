6402351a281e3c88251b705675c5b8cc63abb6e9
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-10-11 20:30:06 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,2 @@
-//it contains the URL of database 
-const URL = 'https://raw.githubusercontent.com/SauravRawat016/Api/main/api.json';
-export default URL;
+const url="https://raw.githubusercontent.com/ShubhamPal23/productsapi/refs/heads/main/api.json";
+export default url;
