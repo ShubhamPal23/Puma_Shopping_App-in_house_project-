58aa74fb23cc137896d21ccab9f375ad235b3d7c
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-10-11 20:27:27 +0530

    Update card1.jsx

--- src/components/card1.jsx
+++ src/components/card1.jsx
@@ -1,34 +1,26 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-//import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Buttons from './buttons';
+import React from "react";
+import cart from "../assets/cart.svg"
+import Buttons from "./buttons";
+import "./Card1.css";
 
+const Card1 = ({ id, name, price, url, fn }) => {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="cardimg">
+          <img className="card-image" src={url} alt={name} />
+        </div>
 
-const Card1=({id,name, desc,url,fn})=>{
-    return (
-        <div style={{margin: '4%'}} >
-    <Card sx={{ maxWidth: 250 }}>
-      <CardMedia
-        component="img"
-        height="170"
-        image={url}/>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        {desc}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Buttons name={"Add To Cart"} pizza_id={id} fn={fn} val="+"/>
-      </CardActions>
-    </Card>
+        <div className="card-content">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-price">Price: Rs.{price}</p>
+        </div>
+        <div className="card-actions">
+          <Buttons name={<span>Add to  <img src={cart} alt="cart" /></span>} prod_id={id} fn={fn} val="+" />
+        </div>
+      </div>
     </div>
-    );
-}
+  );
+};
+
 export default Card1;
