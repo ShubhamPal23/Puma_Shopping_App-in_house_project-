50b3522b48c29d0c1f316d4f21f32f691c5aa991
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-10-11 20:28:36 +0530

    Update message.jsx

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,8 +1,24 @@
-import { colors } from "@mui/material"
-import { Css, Info } from "@mui/icons-material";
-const Message=({classname="Message",val,heading=1})=>{
-    if(heading===1)return(<h1 className="text-center" font-color="White" bgcolor="blue" >{classname}{val}</h1>);
-    else if(heading===2)return(<h2 align="center" >{classname}{val}</h2>);
-    else if(heading===3)return(<h3 align="center">{classname}{val}</h3>);
-}
-export default Message;
+const Message = ({ classname = "Message", val, heading = 1 }) => {
+    const style = { textAlign: "center" }; // Common style for center alignment
+  
+    if (heading === 1) {
+      return <h1 style={style}>{classname}</h1>;
+    } else if (heading === 2) {
+      return (
+        <h2 style={style}>
+          {classname} {val}
+        </h2>
+      );
+    } else if (heading === 5) {
+      return (
+        <h5 style={style}>
+          {classname} {val}
+        </h5>
+      );
+    }
+  
+    return null;
+  };
+  
+  export default Message;
+  
